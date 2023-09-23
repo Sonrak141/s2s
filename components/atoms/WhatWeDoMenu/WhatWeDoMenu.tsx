93fc95762605b692735f3c3ce279cb94f2a98be7
Carlos Borrero <53616525+Sonrak141@users.noreply.github.com>
2023-09-23 13:01:34 -0500

    Video

--- components/atoms/WhatWeDoMenu/WhatWeDoMenu.tsx
+++ components/atoms/WhatWeDoMenu/WhatWeDoMenu.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function WhatWeDoMenu() {
   return (
-    <div className='flex md:text-center mt-2 w-screen md:rounded-md md:bg-white md:text-black flex-col md:flex-row items-center justify-around'>
+    <div className='flex md:text-center text-sm mt-2 w-screen md:rounded-md md:bg-white md:text-black flex-col md:flex-row items-center justify-around'>
       <Link
         href='/defence'
         className='pt-2 pb-2 hover:bg-[#FF0000] hover:text-white px-8 rounded-sm '
